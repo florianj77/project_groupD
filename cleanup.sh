eb84bfe5074b78e9b4a354e0f13ebb3f994c2fd8
Florian Joerg <florian.joerg@aon.at>
2019-10-25 20:37:38 +0200

    clenaup.sh can now make a file with only high QUality results

--- cleanup.sh
+++ cleanup.sh
@@ -1,19 +1,53 @@
 #!/bin/bash
+
+#just temporary files
 touch temporary_file.csv
 tmpFile=temporary_file.csv
+
+#decide which file to clenaup
+echo "The file of which city do you want to cleanup?(Please enter just the city name: [City])"
+read file
+echo $file
+
+if [ $file != "Karlstad" ]; then
+	datafile=smhi-opendata_$file.csv
+	echo "Your are working with" $datafile
+else datafile=smhi-openda_$file.csv
+	echo "Your are working with" $datafile
+fi
+
+if [[ ! $file ]]; then
+	echo "Couldnt find file"
+	exit 1
+fi
+
 #remove old file if it exists
-if [[ cleaned_Lund.csv ]]; then
-	echo "Deleting old file"
-	rm cleaned_Lund.csv
+if [[ cleaned_${file}.csv ]]; then
+	echo "Deleting old file csx"
+	rm cleaned_$file.csv
 fi
 
 #creating header for cleaned file
-echo "Creating header for cleaned_Lund.csv data file"
-echo "\"Date\",\"Time\",\"Temperatur\",\"Quality\"" > cleaned_Lund.csv
+echo "Creating header for cleaned_$file.csv data file"
+echo "\"Date\",\"Time\",\"Temperatur\",\"Quality\"" > cleaned_$file.csv
 
 #selecting only lines which start with YYYY-MM-DD followed by a semicolon
-egrep ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\; smhi-opendata_Lund.csv > ${tmpFile}
-#printing only the date, time, temperatur and quality seperated by a ; to the file
-awk -F ";" '{print $1";"$2";"$3";"$4}' ${tmpFile}>>cleaned_Lund.csv
+egrep ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\; $datafile > ${tmpFile}
 
+#printing only the date, time, temperatur and quality seperated by a ; to the file
+awk -F ";" '{print $1";"$2";"$3";"$4}' ${tmpFile}>>cleaned_$file.csv
+
+#ask if one wants to make a file with only high quality results
+echo "Do you want to make a file with only high Quality results?[y/n]"
+read input
+if input==y; then
+if [[ cleaned_${file}_highQuality.csv ]]; then
+	echo "Deleting old file"
+	rm cleaned_${file}_highQuality.csv
+fi
+echo "Removed"
+fgrep "Y" cleaned_${file}.csv | wc -l
+echo "low Quality entries"
+grep "G" cleaned_${file}.csv >> cleaned_${file}_highQuality.csv
+fi
 rm $tmpFile
