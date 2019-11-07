6a405889d3d401d68b03ac26a576d91a66809377
jakobhuhn <jakob.huhn@campus.lmu.de>
2019-11-07 16:31:52 +0100

    changes the for loops to while loops, this is necessary as a moon day is not exactly 28 days long. Also the main for loop starting at line 17 is not dependent on the moonphases_2014-txt file - the prgram could handle more data.

--- moonTemp.sh
+++ moonTemp.sh
@@ -12,8 +12,9 @@ output=moonTemp_${city}.txt
 Date=`awk 'NR==2 {print $1}' moonphases_2014.txt`
 final_date=`awk 'END {print $1}' moonphases_2014.txt`
 #next_date="2014-01-16"
-NR=`awk 'END{print NR}' moonphases_2014.txt`
-for i in $(seq 0 11)
+lines=`awk 'END{print NR}' moonphases_2014.txt`
+range=`awk -v lines=${lines} 'BEGIN{print (lines-2)/2}'`
+for i in $(seq 0 $range)
 do
 	echo "i= "${i}
 	next_date=`awk -v pattern="${Date}" '$0 ~ pattern {getline; print}' moonphases_2014.txt`
@@ -29,30 +30,36 @@ do
 	#maximum=`awk -v start="${Date}" -v stop="${test2}" 'BEGIN{a=-100} $1 == start,$1 == stop {if ($2>a) {a=$2} fi} END{print a}' oneDayTemp_allEntries_${city}.txt`
 	#echo ${minimum}
 	#echo ${maximum}
-	for j in $(seq 0 28)
+	count=0
+	#for j in $(seq 0 28) ##while
+	following_day=$(date +%Y-%m-%d -d "$Date +$count day")
+	while [[ "${following_day}" < "${new_moon}" ]]
 	do 
-		following_day=$(date +%Y-%m-%d -d "$Date +$j day")
+		following_day=$(date +%Y-%m-%d -d "$Date +$count day")
 		egrep ^${following_day} oneDayTemp_allEntries_${city}.txt>>testfile_$i.txt	
+		count=$((count+1))
 	done
 	minimum=`awk 'BEGIN{a=100}  {if ($2<a) {a=$2} fi} END{print a}' testfile_$i.txt`
 	maximum=`awk 'BEGIN{a=-100} {if ($2>a) {a=$2} fi} END{print a}' testfile_$i.txt`
 	echo ${minimum}
 	echo ${maximum}
-	for k in $(seq 0 28)
+	#for k in $(seq 0 28)
+	count=0
+	following_day=$(date +%Y-%m-%d -d "$Date +$count day")
+	while [[ "${following_day}" < "${new_moon}" ]] 
 	do 
-		following_day=$(date +%Y-%m-%d -d "$Date +$k day")
+		following_day=$(date +%Y-%m-%d -d "$Date +$count day")
 		#egrep ^${following_day} oneDayTemp_allEntries_${city}.txt>>testfile_$i.txt
-		awk -v min="${minimum}" -v max="${maximum}" -v day="${following_day}" '$0 ~ day {print $1" "(($2-min)/(max-min))}' testfile_${i}.txt >> moonTemp_${city}.txt	
+		awk -v min="${minimum}" -v max="${maximum}" -v day="${following_day}" '$0 ~ day {print $1" "(($2-min)/(max-min))}' testfile_${i}.txt >> moonTemp_${city}.txt
+		count=$((count+1))	
 	done
-	
+	rm testfile_${i}.txt
 	#grep "${Date}" oneDayTemp_allEntries_${city}.txt>>testfile.txt
 	#awk -v min="${minimum}" -v max="${maximum}" -v start="${Date}" -v stop="${test2}" '$1 == start,$1 == stop {print $1" "(($2-min)/(max-min))}' oneDayTemp_allEntries_${city}.txt >> moonTemp_${city}.txt
 	#awk -v min="${minimum}" -v max="${maximum}" -v start="${Date}" -v stop="${test2}" '$1 == start,$1 == stop {print $1" "(($2-min)/(max-min))}' testfile_${i}.txt >> moonTemp_${city}.txt
 	#awk -v min="${minimum}" -v max="${maximum}" -v start="${Date}" -v stop="${test2}" '$1 == start,$1 == stop {print $1" "(($2-min)/(max-min))}' testfile_${i}.txt >> moonTemp_${city}.txt
 	#awk -v start="${Date}" -v stop="${next_date}" 
 	#awk 'NR == 1 {print $0" "$0}' moonphases_2014.txt >> moonTemp_${city}.txt
-	rm testfile_$i.txt
-	Date=${new_moon}
 done
 
 ##Edit: changed the code a little bit, now it works to make the 
