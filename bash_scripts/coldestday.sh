f7ac091ef5f6a02bd0dd8d76051f159a744b7186
jakobhuhn <jakob.huhn@campus.lmu.de>
2019-11-09 22:06:56 +0100

    Worked with Flo: filenames corrected

--- bash_scripts/coldestday.sh
+++ bash_scripts/coldestday.sh
@@ -6,26 +6,25 @@ Quality=$2
 path=../data_files
 
 if [[ ${Quality} == "highQuality" ]]; then
-	if [[ -f ${path}/coldestday_HQ_$city.txt ]]; then
-		rm ${path}/coldestday_HQ_$city.txt
-		echo "1"
+	if [[ -f ${path}/coldestday_highQuality_$city.txt ]]; then
+		rm ${path}/coldestday_highQuality_$city.txt
 	fi
 	Date=`awk '$1~/-01-01/ {print $1; exit 1}' ${path}/oneDayTemp_highQuality_$city.txt`
 	NR=`awk 'END{print NR}' ${path}/oneDayTemp_highQuality_$city.txt`
 	next_date=$(date +%Y-%m-%d -d "$Date +$i year")
 	final_date=`awk 'END {print $1}' ${path}/oneDayTemp_highQuality_$city.txt`
 	echo "Creating coldestday_HQ_$city.txt data file, this takes a few seconds"
-	touch ${path}/coldestday_HQ_$city.txt
+	touch ${path}/coldestday_highQuality_$city.txt
 	while [[ "${Date}" < "${final_date}" ]]
 	do
 		next_date=$(date +%Y-%m-%d -d "$Date +$i year")
 		stop_date=$(date +%Y-%m-%d -d "$next_date -1 day")
-		awk -v start="${Date}" -v stop="${stop_date}" 'BEGIN{a=100; b=substr($1,1,4)} $1 == start,$1 == stop {if ($2<a) {a=$2; b=substr($1,1,4)} fi} END{print b" "a}' ${path}/oneDayTemp_highQuality_$city.txt >> ${path}/coldestday_HQ_$city.txt
+		awk -v start="${Date}" -v stop="${stop_date}" 'BEGIN{a=100; b=substr($1,1,4)} $1 == start,$1 == stop {if ($2<a) {a=$2; b=substr($1,1,4)} fi} END{print b" "a}' ${path}/oneDayTemp_highQuality_$city.txt >> ${path}/coldestday_highQuality_$city.txt
 		Date=${next_date}
 	done
 else
-	if [[ -f ${path}/coldestday_$city.txt ]]; then
-		rm ${path}/coldestday_$city.txt
+	if [[ -f ${path}/coldestday_allEntries_$city.txt ]]; then
+		rm ${path}/coldestday_allEntries_$city.txt
 	fi
 	
 	Date=`awk '$1~/-01-01/ {print $1; exit 1}' ${path}/oneDayTemp_allEntries_$city.txt`
@@ -33,12 +32,12 @@ else
 	next_date=$(date +%Y-%m-%d -d "$Date +$i year")
 	final_date=`awk 'END {print $1}' ${path}/oneDayTemp_allEntries_$city.txt`
 	echo "Creating coldestday_$city.txt data file, this takes a few seconds"
-	touch ${path}/coldestday_$city.txt
+	touch ${path}/coldestday_allEntries_$city.txt
 	while [[ "${Date}" < "${final_date}" ]]
 	do
 		next_date=$(date +%Y-%m-%d -d "$Date +$i year")
 		stop_date=$(date +%Y-%m-%d -d "$next_date -1 day")
-		awk -v start="${Date}" -v stop="${stop_date}" 'BEGIN{a=100; b=substr($1,1,4)} $1 == start,$1 == stop {if ($2<a) {a=$2; b=substr($1,1,4)} fi} END{print b" "a}' ${path}/oneDayTemp_allEntries_$city.txt >> ${path}/coldestday_$city.txt
+		awk -v start="${Date}" -v stop="${stop_date}" 'BEGIN{a=100; b=substr($1,1,4)} $1 == start,$1 == stop {if ($2<a) {a=$2; b=substr($1,1,4)} fi} END{print b" "a}' ${path}/oneDayTemp_allEntries_$city.txt >> ${path}/coldestday_allEntries_$city.txt
 		Date=${next_date}
 	done	
 fi
