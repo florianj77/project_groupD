8f2dd12979a6ff69f8224725a2647dcc2536c0fc
jakobhuhn <jakob.huhn@campus.lmu.de>
2019-11-09 13:27:33 +0100

    fixed bug where the year wasnt shown

--- coldestday.sh
+++ coldestday.sh
@@ -37,7 +37,7 @@ else
 	do
 		next_date=$(date +%Y-%m-%d -d "$Date +$i year")
 		stop_date=$(date +%Y-%m-%d -d "$next_date -1 day")
-		awk -v start="${Date}" -v stop="${stop_date}" 'BEGIN{a=100; b=substr($1,1,4)} $1 == start,$1 == stop {if ($2<a) {a=$2; substr($1,1,4)} fi} END{print b" "a}' oneDayTemp_allEntries_$city.txt >> coldestday_$city.txt
+		awk -v start="${Date}" -v stop="${stop_date}" 'BEGIN{a=100; b=substr($1,1,4)} $1 == start,$1 == stop {if ($2<a) {a=$2; b=substr($1,1,4)} fi} END{print b" "a}' oneDayTemp_allEntries_$city.txt >> coldestday_$city.txt
 		Date=${next_date}
 	done	
 fi
